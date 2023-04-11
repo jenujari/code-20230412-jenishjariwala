e6fe9739734a047df5a988c65358a4c025828172
Jenish Jariwala <jhon.5456@gmail.com>
2023-04-11 15:37:56 +0530

    About us

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./assets/styles/index.scss";
 
 import CallOut from "./components/callout";
 import Explore from "./components/explore";
+import AboutUs from "./components/aboutus";
 
 function App() {
   return (
     <main className="boundary">
       <CallOut />
       <Explore />
+      <AboutUs />
     </main>
   );
 }
---
+++ src/components/aboutus.jsx
@@ -0,0 +1,54 @@
+import chapter1 from "./../assets/images/chapter_1.jpg";
+import chapter2 from "./../assets/images/chapter_2.jpg";
+import chapter3 from "./../assets/images/chapter_3.jpg";
+import chapter4 from "./../assets/images/chapter_4.jpg";
+
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-box">
+    <p className="heading">About Us</p>
+    <p className="sub-title">Allow us to tell you a short story...</p>
+    <div className="galary">
+      <div className="galary-item" style={{backgroundImage:`url(${chapter1})`}} >
+        <div className="banner">
+          <p className="title">Chapter I</p>
+          <p className="sub-title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididun
+          </p>
+        </div>
+      </div>
+      <div className="galary-item" style={{backgroundImage:`url(${chapter2})`}} >
+        <div className="banner">
+          <p className="title">Chapter 2</p>
+          <p className="sub-title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididun
+          </p>
+        </div>
+      </div>
+      <div className="galary-item" style={{backgroundImage:`url(${chapter3})`}} >
+        <div className="banner">
+          <p className="title">Chapter 3</p>
+          <p className="sub-title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididun
+          </p>
+        </div>
+      </div>
+      <div className="galary-item" style={{backgroundImage:`url(${chapter4})`}} >
+        <div className="banner">
+          <p className="title">Chapter 4</p>
+          <p className="sub-title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididun
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default AboutUs;
